8d42e11c74268014c60bd618d8e672719015f266
stijnAvdL <stijn.albersvanderlinden@gmail.com>
2015-09-17 15:13:47 +0200

    Added version constraints. Bumped version number

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
     name: "orikami:react-semantic-ui",
-    version: "0.0.1",
+    version: "0.0.2",
     summary: "react-semantify wrapper package",
     git: "https://github.com/dreamyourweb/react-semantic-ui.git",
     documentation: "README.md"
@@ -14,8 +14,8 @@ Package.onUse(function(api) {
     api.versionsFrom("1.2-rc.4");
 
     api.use([
-        "react",
-        "cosmos:browserify"
+        "react@0.1.9",
+        "cosmos:browserify@0.5.0"
     ]);
 
     api.addFiles(["app.browserify.js"]);
